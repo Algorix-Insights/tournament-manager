7448359ce410a0e1ed7ca7306f1be2da0ec0a5d0
ikabeee <carlglz30@gmail.com>
2026-09-12 16:41:56 -0500

    feat: add application routing configuration

--- apps/web/src/features/genres/pages/GenresPage.tsx
+++ apps/web/src/features/genres/pages/GenresPage.tsx
@@ -1,7 +1,7 @@
-export default function PlayersPage() {
+export default function GenresPage() {
   return (
     <section className="rounded-lg border border-slate-800 p-6">
-      <h2 className="text-xl font-semibold">Players</h2>
+      <h2 className="text-xl font-semibold">Genres</h2>
     </section>
   )
 }
