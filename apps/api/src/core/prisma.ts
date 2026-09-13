e39db3775685f91ae748d4d5c4a3e5b79d32e3ec
ikabeee <carlglz30@gmail.com>
2026-09-12 20:48:50 -0500

    refactor: remove fallback database URL in Prisma client initialization

--- apps/api/src/core/prisma.ts
+++ apps/api/src/core/prisma.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from '../generated/prisma/client';
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
 const adapter = new PrismaMariaDb(
-  process.env.DATABASE_URL ?? 'mysql://root:password@localhost:3306/tournament_db'
+  process.env.DATABASE_URL!
 );
 
 export const prisma = new PrismaClient({ adapter });
