5646a7145c56215dbc2b88aa8eaf03af0803bb96
ZurisaraiDR <zurisarai250@gmail>
2026-09-13 23:26:43 -0500

    refactor(components): add style in ArrowButton component

--- apps/web/src/core/ui/ArrowButton.tsx
+++ apps/web/src/core/ui/ArrowButton.tsx
@@ -1,7 +1,7 @@
 export default function ArrowButton({ children }: { children: string }) {
   return (
     <button
-      className="flex h-9 items-center justify-between rounded-full bg-[#101827] py-1 pl-5 pr-1 text-[14px] text-white transition-transform hover:scale-[1.02]"
+      className="flex h-9 items-center justify-between rounded-full bg-[#101827] py-1 pl-5 pr-1 text-[14px] text-white transition-transform hover:scale-[1.02] cursor-pointer"
       type="button"
     >
       
@@ -10,7 +10,7 @@ export default function ArrowButton({ children }: { children: string }) {
   </span>
       <span
         aria-hidden="true"
-        className="flex size-7 items-center justify-center rounded-full bg-[#f4f1f8] text-[14px] text-[#101827]"
+        className="flex size-7 items-center justify-center rounded-full bg-[#f4f1f8] text-[14px] text-[#101827] "
       >
         →
       </span>
